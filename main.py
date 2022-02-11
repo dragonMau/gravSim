fc8612582f58d0a8e9f87b8b6bfde5b4d2410e60
dragonMau <73312635+dragonMau@users.noreply.github.com>
2022-02-11 14:26:29 +0200

    small code refactor, ordered sin and cos.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ Gravity Simulator
 description: It represents Newton's law of universal gravitation by
 simulating some planets and using his equation on them.
 
-version: 1.1 - beta
+version: 1.4 - beta
 author: DisFunSec
 """
 from tkinter import Canvas, Label, Tk
@@ -11,29 +11,6 @@ from sys import argv
 from json import loads
 from os.path import isdir, isfile
 from os import listdir
-
-#  load config from json
-if len(argv) > 1:
-    if isfile(argv[1]):
-        with open(argv[1], "r") as tf:
-            config = loads(tf.read())  # any file by given path
-    elif isdir(argv[1]):
-        files = [file for file in listdir(argv[1]) if isfile(file)]
-        if "config.json" in files:
-            with open(argv[1], "r") as tf:
-                config = loads(tf.read())  # 'config.json' on given directory
-        else:
-            raise Exception(f"not found \"config.json\" in \"{argv[1]}\"")
-    else:
-        raise Exception(f"invalid path \"{argv[1]}\"")
-elif isfile("./config.json"):
-    with open(argv[1], "r") as tf:
-        config = loads(tf.read())  # default configuration
-else:
-    raise Exception("not found \"config.json\"")
-
-# configure max frame rate
-frame_rate = config["other"]["frame rate"]
 
 
 # make point data type
@@ -48,21 +25,17 @@ class Point:
 
 # make Gravity calculator
 class Gravity:
-    # configure grav_const
-    grav_const = config["other"]["gravity const"]
+    grav_const = 6.67 * 10 ** -11
 
     # method that takes distance and mass and returns gravity force
-    def equation(self, dist=1.0, m1=1.0):
+    @staticmethod
+    def equation(dist=1.0, m1=1.0):
         if dist > 0:
-            return (self.grav_const * m1) / dist ** 2
+            return (Gravity.grav_const * m1) / dist ** 2
         elif dist == 0:
             return 0
         else:
             raise Exception(f"Impossible args: dist={dist}, m1={m1}.")
-
-
-# init Gravity calculator
-gravity = Gravity()
 
 
 # make vector data type
@@ -79,14 +52,29 @@ class Vector:
             self.cos = x / self.len
             self.sin = y / self.len
         except ZeroDivisionError:  # even math isn't perfect
-            self.sin = 1
+            self.sin = 0
             self.cos = 1
 
-    def get(self):  # ok
-        return self.cos, self.sin, self.len
+    def get(self, *whats):  # ok
+        if whats == ():
+            return self.cos, self.sin, self.len
+        else:
+            return_ = ()
+            for what in whats:
+                if what in ["sin", "cos", "len"]:
+                    return_ += (self.__getattribute__(what),)
+                elif what == "x":
+                    return_ += (self.cos * self.len,)
+                elif what == "y":
+                    return_ += (self.sin * self.len,)
+            if len(return_) == 1:
+                return return_[0]
+            return return_
+
+    def __add__(self, v2):
+        return Vector(Point((self.get("x") + v2.get("x")), (self.get("y") + v2.get("y"))))
 
 
-#  this is shittiest piece of code
 class Display:
     canvas: Canvas
     label: Label
@@ -99,7 +87,7 @@ class Display:
     def update_pos(self, event):  # update label with position of mouse on map
         tx = self.cam.x + (event.x - 500) / self.cam.zoom
         ty = self.cam.y + (event.y - 250) / self.cam.zoom
-        self.root.title(f"GravSim v1.1  x: {tx} y: {ty}")
+        self.root.title(f"GravSim v1.4  x: {tx} y: {ty}")
 
     def zoom_event(self, e):  # zooming(mouse wheel scrolling)
         self.cam.zoom *= 1.2 ** (e.delta / 120)
@@ -112,15 +100,16 @@ class Display:
         self.cam.y = event.y / self.cam.zoom + self.drag_pos.y
         self.update_pos(event)
 
-    def __init__(self):  # tkinter sus
+    def __init__(self, frame_rate):  # tkinter sus
         self.root = Tk()
-        self.root.title("GravSim v1.1  x: 0 y: 0")
+        self.root.title("GravSim v1.4  x: 0 y: 0")
         self.canvas = Canvas(self.root, width=1000, height=500, bg="#000022")
         self.canvas.pack()
         self.canvas.bind('<Button-1>', self.move_start)
         self.canvas.bind('<B1-Motion>', self.move_cont)
         self.canvas.bind('<MouseWheel>', self.zoom_event)
         self.canvas.bind('<Motion>', self.update_pos)
+        self.frame_rate = frame_rate
 
     # starting function
     def lp1(self, cam):
@@ -154,17 +143,13 @@ class Display:
             self.bodies[b].move()  # move it
             vector = Vector(Point(0, 0))
             for p1 in self.bodies:  # sum every force that pulls/pushes the body
-                direction = Vector(Point(body.pos.y - p1.pos.y, body.pos.x - p1.pos.x))  # direction
-                direction.len = gravity.equation(direction.len, p1.mass)  # force
-                vector = add_v(vector, direction)
+                direction = Vector(Point(body.pos.x - p1.pos.x, body.pos.y - p1.pos.y))  # direction
+                direction.len = Gravity.equation(direction.len, p1.mass)  # force
+                vector += direction
             self.bodies[b].accelerate(vector)  # add to its velocity vector
 
-        self.root.after(frame_rate, self.lp)  # loop main loop
-
-
-# method to add vectors together, yea, maybe batter it will be on vector class, but...
-def add_v(v1: Vector, v2: Vector):
-    return Vector(Point((v1.cos * v1.len + v2.cos * v2.len), (v1.sin * v1.len + v2.sin * v2.len)))
+        # input()
+        self.root.after(self.frame_rate, self.lp)  # loop main loop
 
 
 # make body data type
@@ -185,11 +170,12 @@ class Body:
         return self.mass, self.vel, self.pos
 
     def move(self):
-        self.pos.y -= self.vel.cos * self.vel.len
-        self.pos.x -= self.vel.sin * self.vel.len
+        self.pos.y -= self.vel.get("y")
+        self.pos.x -= self.vel.get("x")
 
     def accelerate(self, force_v: Vector):
-        self.vel = add_v(self.vel, force_v)
+        self.vel = self.vel + force_v
+        # print(self.tag, force_v.get("x", "y"))
 
 
 class Main:
@@ -197,21 +183,48 @@ class Main:
     y: float
     zoom: float
 
-    def __init__(self):  # load other configurations
-        self.x = float(config["camera_position"]["x"])
-        self.y = float(config["camera_position"]["y"])
-        self.zoom = float(config["camera_position"]["zoom"])
+    @staticmethod
+    def load_config():
+        if len(argv) > 1:
+            if isfile(argv[1]):
+                with open(argv[1], "r") as tf:
+                    config = loads(tf.read())  # any file by given path
+            elif isdir(argv[1]):
+                files = [file for file in listdir(argv[1]) if isfile(file)]
+                if "config.json" in files:
+                    with open(argv[1], "r") as tf:
+                        config = loads(tf.read())  # 'config.json' on given directory
+                else:
+                    raise Exception(f"not found \"config.json\" in \"{argv[1]}\"")
+            else:
+                raise Exception(f"invalid path \"{argv[1]}\"")
+        elif isfile("./config.json"):
+            with open(argv[1], "r") as tf:
+                config = loads(tf.read())  # default configuration
+        else:
+            raise Exception("not found \"config.json\"")
+        return config
 
-        self.disp = Display()
-        for planet in config["planets"]:  # append all planets
+    def __init__(self):  # load other configurations
+        self.config = self.load_config()
+
+        self.x = float(self.config["camera_position"]["x"])
+        self.y = float(self.config["camera_position"]["y"])
+        self.zoom = float(self.config["camera_position"]["zoom"])
+
+        Gravity.grav_const = self.config["other"]["gravity const"]
+        self.disp = Display(self.config["other"]["frame rate"])
+        for planet in self.config["planets"]:  # append all planets
             self.disp.bodies.append(Body(
                 mass=planet["mass"],
                 pos=Point(planet["pos"]["x"], planet["pos"]["y"]),
                 color=planet["color"],
                 vel=Vector(Point(planet["vel"]["dx"], planet["vel"]["dy"])))
             )
+
+    def start(self):
         self.disp.lp1(self)  # start
 
 
 if __name__ == '__main__':
-    Main()
+    Main().start()
